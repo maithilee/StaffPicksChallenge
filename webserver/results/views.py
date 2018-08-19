451fbfd51549e6375b28a20287ab0e2e39698c24
maithilee <maithi1@umbc.edu>
2018-08-18 23:06:22 -0400

    Handle situation for clip Id not in database

--- webserver/results/views.py
+++ webserver/results/views.py
@@ -6,6 +6,7 @@ import os
 PROJECT_DIR = os.getcwd()
 sys.path.insert(0, PROJECT_DIR)
 from dataReady2 import belongsTo, categories, titles, captns, image
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -13,21 +14,25 @@ def home(request):
 
 def compute(request):
     # Load the saved model
-    model = Doc2Vec.load("ClipModel")
-    clipid = request.GET.get('clipid', '')
+    try:
+        model = Doc2Vec.load("ClipModel")
+        clipid = request.GET.get('clipid', '')
 
-    results = []
-    # Find 10 most similar clips
-    similar_doc = model.docvecs.most_similar(positive=[clipid], topn=10)
-    for simDoc in similar_doc:
-        clipId = simDoc[0]
-        doc = {}
-        doc['id'] = clipId
-        catNames = [categories[str(catid)] for catid in belongsTo[clipId]]
-        doc['title'] = titles[clipId]
-        doc['captions'] = str(captns[clipId]).replace('\r\n', ' ')
-        doc['categories'] = catNames
-        doc['image'] = image[clipId]
-        results.append(doc)
-        jresults = json.dumps(results, indent=4)
-    return render_to_response('viewResults.html', {'result' : jresults})
+        results = []
+        # Find 10 most similar clips
+        similar_doc = model.docvecs.most_similar(positive=[clipid], topn=10)
+        for simDoc in similar_doc:
+            clipId = simDoc[0]
+            doc = {}
+            doc['id'] = clipId
+            catNames = [categories[str(catid)] for catid in belongsTo[clipId]]
+            doc['title'] = titles[clipId]
+            doc['captions'] = str(captns[clipId]).replace('\r\n', ' ')
+            doc['categories'] = catNames
+            doc['image'] = image[clipId]
+            results.append(doc)
+            jresults = json.dumps(results, indent=4)
+        return render_to_response('viewResults.html', {'result' : jresults})
+    except:
+        html = "<html> <body>Clip ID not found in database </body></html>"
+        return HttpResponse(html)
